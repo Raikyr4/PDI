de264ce6258c0cb6a570948640713c66b5ce5309
raiky.novaes <raiky.novaes@lg.com.br>
2026-02-26 19:10:42 -0300

    first commit

---
+++ LAB01/LeituraSeparacaoDeCanaisRGB.py
@@ -0,0 +1,19 @@
+
+# ###**Laboratório 01** 
+# **Leitura, Exibição e Separação de Canais RGB**
+# **Objetivo:** Praticar a leitura de arquivos JPEG e entender a decomposição de cores no espaço RGB.
+
+# ---
+# 1 - Crie um script em Python utilizando a biblioteca OpenCV (cv2) que realize a leitura de uma imagem no formato JPEG 
+# (utilize uma das imagens sugeridas).
+
+# 2 - Exiba a imagem original colorida em uma janela com um título apropriado.
+
+# 3 - Separe a imagem colorida em seus três canais componentes: Vermelho (R), Verde (G) e Azul (B). Lembre-se que o 
+# OpenCV tipicamente lê imagens no formato BGR.
+
+# 4 - Crie e exiba três imagens em escala de cinza separadas, onde cada imagem representa a intensidade do respectivo 
+# canal (R, G e B). Dica: Ao exibir o canal, certifique-se de que ele esteja representado como uma imagem de 8 bits 
+# em escala de cinza para visualização correta.
+
+# 5 - Adicione um texto descritivo (título da janela) para cada canal exibido 
---
+++ LAB02/Lab02.py
@@ -0,0 +1,21 @@
+### **Laboratório 02**
+
+# **Conversão e Análise no Espaço de Cores HSV**
+
+# **Objetivo:** Aplicar a conversão para o espaço de cores HSV e visualizar seus componentes.
+
+# ---
+
+# 1 - Partindo da imagem lida no Laboratório 01, converta-a do espaço de cores RGB (ou BGR, conforme lida pelo OpenCV) 
+# para o espaço de cores HSV (Tonalidade - H, Saturação - S, Valor/Brilho - V).
+
+# 2 - Separe a imagem convertida nos seus três canais componentes: H, S e V.
+
+# 3 - Exiba os três canais HSV separadamente, também como imagens em escala de cinza.
+
+# 4 - Com base na imagem original (por exemplo, a imagem com cores vivas sugerida), analise e descreva no código,
+#  em forma de comentários, o que cada canal (H, S e V) representa visualmente:
+
+# - O que as áreas claras no canal H indicam?
+# - O que as áreas claras no canal S indicam?
+# - O que o canal V representa em relação à imagem original?
